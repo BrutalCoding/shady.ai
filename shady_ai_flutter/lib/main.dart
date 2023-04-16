e56a92f7e3b881a3c0ae438432a20dea5402671f
BrutalCoding <daniel@brutalcoding.com>
2023-04-16 15:40:45 +0800

    fix: Unused code

--- shady_ai_flutter/lib/main.dart
+++ shady_ai_flutter/lib/main.dart
@@ -1,18 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:serverpod_flutter/serverpod_flutter.dart';
-import 'package:shady_ai_client/shady_ai_client.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-// Sets up a singleton client object that can be used to talk to the server from
-// anywhere in our app. The client is generated from your server code.
-// The client is set up to connect to a Serverpod running on a local server on
-// the default port. You will need to modify this to connect to staging or
-// production servers.
-var client = Client('http://localhost:8080/')
-  ..connectivityMonitor = FlutterConnectivityMonitor();
-
 void main() {
-  // final onnyxSessiojn = bindings.
   runApp(const Main());
 }
 
